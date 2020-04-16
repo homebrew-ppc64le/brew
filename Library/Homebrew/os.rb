337b77b8e1466a7cfacdf6361e50e1b359a33204
Trung Lê <trung.le@ruby-journal.com>
2020-04-16 16:36:13 +1000

    Add OS.linux_ppc64le?

--- Library/Homebrew/os.rb
+++ Library/Homebrew/os.rb
@@ -13,6 +13,10 @@ module OS
     RbConfig::CONFIG["host_os"].include? "linux"
   end
 
+  def self.linux_ppc64le?
+    linux? && RbConfig::CONFIG["host_cpu"] == "powerpc64le"
+  end
+
   ::OS_VERSION = ENV["HOMEBREW_OS_VERSION"]
 
   if OS.mac?
